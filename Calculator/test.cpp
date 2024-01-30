5d99f0c1c3b2a05db80d579deb1164870dfaa8c6
T N Sneha <tn.sneha@ltts.com>
2024-01-30 19:58:35 +0530

    Updated test.cpp

--- Calculator/test.cpp
+++ Calculator/test.cpp
@@ -9,6 +9,6 @@ TEST(FirstTestGroup, FirstTest)
 {
 	CHECK_EQUALS(5, add(2, 3));
 	CHECK_EQUALS(8, sub(4, 4));
-	CHECK_EQUALS(10, multi(5, 2));
-	CHECK_EQUALS(2, division(10, 2));
+	CHECK_EQUALS(10, multiply(5, 2));
+	CHECK_EQUALS(2, divide(10, 2));
 }
