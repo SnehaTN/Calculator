0c468409b51fd8335c5136e58a3653ed78f510ad
T N Sneha <tn.sneha@ltts.com>
2024-01-30 16:51:31 +0530

    Added test files

---
+++ Calculator/test.cpp
@@ -0,0 +1,13 @@
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(FirstTestGroup)
+{
+};
+
+TEST(FirstTestGroup, FirstTest)
+{
+	CHECK_EQUALS(5, add(2, 3));
+	CHECK_EQUALS(8, sub(4, 4));
+	CHECK_EQUALS(10, multi(5, 2));
+	CHECK_EQUALS(2, division(10, 2));
+}
