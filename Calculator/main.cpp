64655f707734d26dafbbce59510e42b8cf10dcd0
T N Sneha <tn.sneha@ltts.com>
2024-01-30 16:27:00 +0530

    Updated main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream.h>  
+#include<iostream>  
 #include<stdio.h>  
 #include<conio.h>  
 #include<math.h>  
@@ -12,12 +12,11 @@ void srt();
 void exit();
 void main()
 {
-    clrscr();
     int opr;
     // display different operation of the calculator  
     do
     {
-        cout << "Select any operation from the C++ Calculator"
+        std::cout << "Select any operation from the C++ Calculator"
             "\n1 = Addition"
             "\n2 = Subtraction"
             "\n3 = Multiplication"
@@ -26,7 +25,7 @@ void main()
             "\n6 = Square Root"
             "\n7 = Exit"
             "\n \n Make a choice: ";
-        cin >> opr;
+        std::cin >> opr;
 
         switch (opr)
         {
@@ -52,10 +51,10 @@ void main()
             exit(0);   // terminate the program  
             break;
         default:
-            cout << "Something is wrong..!!";
+            std::cout << "Something is wrong..!!";
             break;
         }
-        cout << " \n------------------------------\n";
+        std::cout << " \n------------------------------\n";
     } while (opr != 7);
     getch();
 }
@@ -63,67 +62,67 @@ void main()
 void add()
 {
     int n, sum = 0, i, number;
-    cout << "How many numbers you want to add: ";
-    cin >> n;
-    cout << "Please enter the number one by one: \n";
+    std::cout << "How many numbers you want to add: ";
+    std::cin >> n;
+    std::cout << "Please enter the number one by one: \n";
     for (i = 1; i <= n; i++)
     {
-        cin >> number;
+        std::cin >> number;
         sum = sum + number;
     }
-    cout << "\n Sum of the numbers = " << sum;
+    std::cout << "\n Sum of the numbers = " << sum;
 }
 void sub()
 {
     int num1, num2, z;
-    cout << " \n Enter the First number = ";
-    cin >> num1;
-    cout << "\n Enter the Second number = ";
-    cin >> num2;
+    std::cout << " \n Enter the First number = ";
+    std::cin >> num1;
+    std::cout << "\n Enter the Second number = ";
+    std::cin >> num2;
     z = num1 - num2;
-    cout << "\n Subtraction of the number = " << z;
+    std::cout << "\n Subtraction of the number = " << z;
 }
 void multi()
 {
     int num1, num2, mul;
-    cout << " \n Enter the First number = ";
-    cin >> num1;
-    cout << "\n Enter the Second number = ";
-    cin >> num2;
+    std::cout << " \n Enter the First number = ";
+    std::cin >> num1;
+    std::cout << "\n Enter the Second number = ";
+    std::cin >> num2;
     mul = num1 * num2;
-    cout << "\n Multiplication of two numbers = " << mul;
+    std::cout << "\n Multiplication of two numbers = " << mul;
 }
 void division()
 {
     int num1, num2, div = 0;
-    cout << " \n Enter the First number = ";
-    cin >> num1;
-    cout << "\n Enter the Second number = ";
-    cin >> num2;
+    std::cout << " \n Enter the First number = ";
+    std::cin >> num1;
+    std::cout << "\n Enter the Second number = ";
+    std::cin >> num2;
     while (num2 == 0)
     {
-        cout << "\n Divisor canot be zero"
+        std::cout << "\n Divisor canot be zero"
             "\n Please enter the divisor once again: ";
-        cin >> num2;
+        std::cin >> num2;
     }
     div = num1 / num2;
-    cout << "\n Division of two numbers = " << div;
+    std::cout << "\n Division of two numbers = " << div;
 }
 void sqr()
 {
     int num1;
     float sq;
-    cout << " \n Enter a number to find the Square: ";
-    cin >> num1;
+    std::cout << " \n Enter a number to find the Square: ";
+    std::cin >> num1;
     sq = num1 * num1;
-    cout << " \n Square of " << num1 << " is : " << sq;
+    std::cout << " \n Square of " << num1 << " is : " << sq;
 }
 void srt()
 {
     float q;
     int num1;
-    cout << "\n Enter the number to find the Square Root:";
-    cin >> num1;
+    std::cout << "\n Enter the number to find the Square Root:";
+    std::cin >> num1;
     q = sqrt(num1);
-    cout << " \n Square Root of " << num1 << " is : " << q;
+    std::cout << " \n Square Root of " << num1 << " is : " << q;
 }
