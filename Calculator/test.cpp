7cdf24b681655e755adb3c1f297e19bb8347d47b
SnehaTN <51622181+SnehaTN@users.noreply.github.com>
2024-01-31 12:15:15 +0530

    Update test.cpp

--- Calculator/test.cpp
+++ Calculator/test.cpp
@@ -10,5 +10,5 @@ TEST(FirstTestGroup, FirstTest)
 	CHECK_EQUAL(5, add(2, 3));
 	CHECK_EQUAL(8, sub(4, 4));
 	CHECK_EQUAL(10, multiply(5, 2));
-	CHECK_EQUAL(2, divide(10, 2));
+	CHECK_EQUAL(3, divide(10, 2));
 }
